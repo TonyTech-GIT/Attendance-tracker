9d32888b07b1ad1abaa97216a84b78b1e13b9cff
TonyTech-GIT <toomgbebu@gmail.com>
2023-09-30 12:37:58 +0100

    resolved build-deployment issues

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 const Welcome = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            {/* <h1>Hello World</h1> */}
             <div className="welcome">
                 <h1>Welcome to the App. Let's get started</h1>
 
